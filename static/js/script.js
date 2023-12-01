e8b513004234ff516fe314847c2a5210bb4312a9
XChiChiX <bob1993.chg@gmail.com>
2023-12-01 22:49:25 +0800

    feat: 美化前端介面

--- static/js/script.js
+++ static/js/script.js
@@ -2,29 +2,32 @@ let startBtn = document.querySelector(".start-btn");
 const downloadBtns = document.querySelectorAll('.download-btn');
 
 startBtn.addEventListener("click", () => {
-    let file = document.getElementById("original-video").files[0];
-    console.log(file)
+    let startConfirm = confirm("確定要啟動ㄇ");
+    if(startConfirm){
+        let file = document.getElementById("original-video").files[0];
+        console.log(file)
 
-    if (file) {
-        const formData = new FormData();
-        formData.append('file', file);
-    
-        // 使用fetch方法將資料傳到後端
-        fetch('/api/upload', {
-            method: 'POST',
-            body: formData,
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
-                downloadBtns.forEach(downloadBtn => {
-                    downloadBtn.style.display = "inline-block";
-                })
+        if (file) {
+            const formData = new FormData();
+            formData.append('file', file);
+        
+            // 使用fetch方法將資料傳到後端
+            fetch('/api/upload', {
+                method: 'POST',
+                body: formData,
             })
-            .catch(error => console.error('Error:', error));
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    downloadBtns.forEach(downloadBtn => {
+                        downloadBtn.style.display = "inline-block";
+                    })
+                })
+                .catch(error => console.error('Error:', error));
         } else {
             console.error('未選擇文件');
         }
+    }
 });
 
 downloadBtns.forEach(downloadBtn => {
